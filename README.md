02b8bd9a3fbf9fb0c78616371d05ea2551591996
zhangb <xiaobo1128@gmail.com>
2015-10-16 15:20:05 +0800

    step 3

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
 $ go test -v
 
-####Race Detector(竞态分析）
+#### Race Detector(竞态分析）
  
 $ go test -v -race
+
+#### Run the benchmarks:
+
+$ go test -v -run=^$ -bench=.
+
+
+#### CPU Profiling
+
+$ go test -v -run=^$ -bench=^BenchmarkHi$ -benchtime=2s -cpuprofile=prof.cpu
+
+会生成prof.cpu和xxx.test两个文件
+
+$ go tool pprof xxx.test prof.cpu
+
+(pprof) top
+
+(pprof) top –cum
+
+(pprof) list handleHi
---
+++ step3/demo.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"regexp"
+	"sync/atomic"
+)
+
+var visitors int64
+
+var rxOptionalID = regexp.MustCompile(`^\d*$`)
+
+func handleHi(w http.ResponseWriter, r *http.Request) {
+
+	if match, _ := regexp.MatchString(`^\w*$`, r.FormValue("color")); !match {
+		http.Error(w, "Optional color is invalid", http.StatusBadRequest)
+		return
+	}
+
+	//visitors++ //此处存在竟争
+	//fix
+	//1.使用channel
+	//2.使用Mutex
+	//3.使用atomic
+
+	visitors := atomic.AddInt64(&visitors, 1)
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Write([]byte(fmt.Sprintf("<h1 style='color:%s'>Welcome!</h1>You are visitor number %d!", r.FormValue("color"), visitors)))
+}
+
+func main() {
+	log.Println("Starting on port 3000")
+	http.HandleFunc("/hi", handleHi)
+	log.Fatal(http.ListenAndServe(":3000", nil))
+}
